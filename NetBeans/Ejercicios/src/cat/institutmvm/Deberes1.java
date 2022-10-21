ff8dce19511fb89010a2f696b90336a93068e8b1
MarkoPareja <mpareja@institutmvm.cat>
2022-10-21 12:31:11 +0200

    Act

---
+++ NetBeans/Ejercicios/src/cat/institutmvm/Deberes1.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.institutmvm;
+
+//Recibir dos numeros y que debuelva la división
+
+public class Deberes1 {
+    
+}
