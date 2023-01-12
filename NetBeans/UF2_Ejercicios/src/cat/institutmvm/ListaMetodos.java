f4fef1dd6505dc313f58a0ebb2aed806d4c16286
MarkoPareja <mpareja@institutmvm.cat>
2023-01-12 13:12:07 +0100

    ACT

--- NetBeans/UF2_Ejercicios/src/cat/institutmvm/ListaMetodos.java
+++ NetBeans/UF2_Ejercicios/src/cat/institutmvm/ListaMetodos.java
@@ -35,35 +35,19 @@ public class ListaMetodos {
         }
     }
 
-    public void randomSearch(int num1) {
-        int min = 15, max = 200, i;
-        int[] arrayint = new int[10];
-        boolean exit = false, quit = false;
-        Scanner sc = new Scanner(System.in);
+    public int random(int max) {
+        int min = 0, num;
 
-        for (i = 0; i < arrayint.length; i++) {
-            arrayint[i] = (int) Math.floor(Math.random() * (max - min + 1) + min);
+        num = (int) Math.floor(Math.random() * (max - min) + min);
+        return num;
+    }
+    
+    public boolean searchNum(int num, int num2) {
+        boolean trobat = false;
+        
+        if(num == num2){
+            trobat = true;
         }
-
-        i = 0;
-        do {
-            if (i < 10) {
-                if (num1 == arrayint[i]) {
-                    exit = true;
-                } else {
-                    exit = false;
-                }
-                i++;
-            } else {
-                exit = true;
-                quit = true;
-            }
-        } while (!quit && !exit);
-
-        if (quit) {
-            System.out.println("El nombre no s'ha trobat a la llista");
-        } else {
-            System.out.println("El nombre s'ha trobat a la llista");
-        }
+        return trobat;
     }
 }
--- NetBeans/UF2_Ejercicios/src/cat/institutmvm/Metodes14.java
+++ NetBeans/UF2_Ejercicios/src/cat/institutmvm/Metodes14.java
@@ -11,14 +11,30 @@ public class Metodes14 {
     private static final String MSG_1 = "Introdueix un nombre";
 
     public static void main(String[] args) {
+        int[] arrayint = new int[10];
+        int num1, num2 = 0, i;
+        boolean trobat = false;
         Scanner sc = new Scanner(System.in);
 
         ListaMetodos method = new ListaMetodos();
 
-        int num1;
-
         num1 = method.valorCorrecte(sc);
-        method.randomSearch(num1);
+        
+        for (i = 0;  i<arrayint.length; i++){
+           num2 = method.random(200);
+           arrayint[i] = num2;
+        }
+        
+        for (i = 0;  i<arrayint.length; i++){
+            System.out.println(arrayint[i]);
+        }
+        
+        i = 0 ;
+        while(!trobat || i == arrayint.length){
+            trobat = method.searchNum(num1, arrayint[i]);
+            i++;
+        }
+        
         sc.close();
     }
 }
