69b24fbe74fafae452da6807d51566ba3ebaae19
MarkoPareja <mpareja@institutmvm.cat>
2023-04-28 10:38:23 +0200

    ACT

---
+++ UF5_Ejercicios/src/cat/institutmvm/MyGrid.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.institutmvm;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashSet;
+import java.util.Set;
+import javax.swing.JComboBox;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+/**
+ *
+ * @author alumne_1r
+ */
+public class MyGrid extends JFrame{
+    public MyGrid(){
+           //<editor-fold desc="Centrar frame">
+        Toolkit screen = Toolkit.getDefaultToolkit();
+        Dimension screenSize = screen.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        setSize(screenWidth / 2, screenHeight / 2);
+        setLocation(screenWidth / 4, screenHeight / 4);
+        this.setResizable(false);
+        //</editor-fold>
+    }
+}
